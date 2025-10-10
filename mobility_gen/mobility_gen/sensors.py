c091814a35ee2e145be6b2c2980f1651bfe374c6
ey-bennett-breese <bennett.breese@ey.com>
2025-10-10 13:35:19 -0400

    mob gen

---
+++ mobility_gen/mobility_gen/sensors.py
@@ -0,0 +1,214 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+from typing import Tuple
+
+
+import omni.replicator.core as rep
+from isaacsim.core.prims import SingleXFormPrim as XFormPrim
+
+
+from omni.ext.mobility_gen.utils.global_utils import get_stage
+from omni.ext.mobility_gen.utils.stage_utils import stage_add_usd_ref
+from omni.ext.mobility_gen.common import Module, Buffer
+
+
+class Sensor(Module):
+
+    def build(self, prim_path: str):
+        raise NotImplementedError
+    
+    def attach(self, prim_path: str):
+        raise NotImplementedError
+
+
+class Camera(Sensor):
+
+    def __init__(self,
+            prim_path: str,
+            resolution: Tuple[int, int]
+        ):
+
+        self._prim_path = prim_path
+        self._resolution = resolution
+        self._render_product = None
+        self._rgb_annotator = None
+        self._segmentation_annotator = None
+        self._instance_id_segmentation_annotator = None
+        self._normals_annotator = None
+        self._depth_annotator = None
+        self._xform_prim = XFormPrim(self._prim_path)
+
+        self.rgb_image = Buffer(tags=["rgb"])
+        self.segmentation_image = Buffer(tags=["segmentation"])
+        self.segmentation_info = Buffer()
+        self.depth_image = Buffer(tags=["depth"])
+        self.instance_id_segmentation_image = Buffer(tags=["segmentation"])
+        self.instance_id_segmentation_info = Buffer()
+        self.normals_image = Buffer(tags=['normals'])
+        self.position = Buffer()
+        self.orientation = Buffer()
+
+    def enable_rendering(self):
+        
+        self._render_product = rep.create.render_product(
+            self._prim_path,
+            self._resolution,
+            force_new=False
+        )
+
+    def disable_rendering(self):
+        if self._render_product is None:
+            return
+        
+        if self._rgb_annotator is not None:
+            self._rgb_annotator.detach()
+            self._rgb_annotator = None
+        
+        if self._segmentation_annotator is not None:
+            self._segmentation_annotator.detach()
+            self._segmentation_annotator = None
+
+        if self._depth_annotator is not None:
+            self._depth_annotator.detach()
+            self._depth_annotator = None
+
+        self._render_product.destroy()
+        self._render_product = None
+    
+    def enable_rgb_rendering(self):
+        if self._render_product is None:
+            self.enable_rendering()
+        if self._rgb_annotator is not None:
+            return
+        self._rgb_annotator = rep.AnnotatorRegistry.get_annotator("LdrColor")
+        self._rgb_annotator.attach(self._render_product)
+
+    def enable_segmentation_rendering(self):
+        if self._render_product is None:
+            self.enable_rendering()
+        if self._segmentation_annotator is not None:
+            return
+        self._segmentation_annotator = rep.AnnotatorRegistry.get_annotator(
+            "semantic_segmentation", init_params=dict(colorize=False)
+        )
+        self._segmentation_annotator.attach(self._render_product)
+
+    def enable_instance_id_segmentation_rendering(self):
+        if self._render_product is None:
+            self.enable_rendering()
+        if self._instance_id_segmentation_annotator is not None:
+            return
+        self._instance_id_segmentation_annotator = rep.AnnotatorRegistry.get_annotator(
+            "instance_id_segmentation", init_params=dict(colorize=False)
+        )
+        self._instance_id_segmentation_annotator.attach(self._render_product)
+
+    def enable_depth_rendering(self):
+        if self._render_product is None:
+            self.enable_rendering()
+        if self._depth_annotator is not None:
+            return
+        self._depth_annotator = rep.AnnotatorRegistry.get_annotator(
+            "distance_to_camera"
+        )
+        self._depth_annotator.attach(self._render_product)
+
+    def enable_normals_rendering(self):
+        if self._render_product is None:
+            self.enable_rendering()
+        if self._normals_annotator is not None:
+            return
+        self._normals_annotator = rep.AnnotatorRegistry.get_annotator(
+            "normals"
+        )
+        self._normals_annotator.attach(self._render_product)
+
+    def update_state(self):
+        if self._rgb_annotator is not None:
+            self.rgb_image.set_value(
+                self._rgb_annotator.get_data()[:, :, :3]
+            )
+        if self._segmentation_annotator is not None:
+            data = self._segmentation_annotator.get_data()
+            seg_image = data['data']
+            seg_info = data['info']
+            self.segmentation_image.set_value(seg_image)
+            self.segmentation_info.set_value(seg_info)
+
+        if self._depth_annotator is not None:
+            self.depth_image.set_value(
+                self._depth_annotator.get_data()
+            )
+
+        if self._instance_id_segmentation_annotator is not None:
+            data = self._instance_id_segmentation_annotator.get_data()
+            id_seg_image = data['data']
+            id_seg_info = data['info']
+            self.instance_id_segmentation_image.set_value(id_seg_image)
+            self.instance_id_segmentation_info.set_value(id_seg_info)
+
+        if self._normals_annotator is not None:
+            data = self._normals_annotator.get_data()
+            self.normals_image.set_value(data)
+            
+        position, orientation = self._xform_prim.get_world_pose()
+        self.position.set_value(position)
+        self.orientation.set_value(orientation)
+        
+        super().update_state()
+
+
+#=========================================================
+#  FINAL CLASSES
+#=========================================================
+
+
+class HawkCamera(Sensor):
+
+    usd_url: str = "http://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/Isaac/4.2/Isaac/Sensors/LeopardImaging/Hawk/hawk_v1.1_nominal.usd"
+    resolution: Tuple[int, int] = (960, 600)
+    left_camera_path: str = "left/camera_left"
+    right_camera_path: str = "right/camera_right"
+
+    def __init__(self, 
+            left: Camera, 
+            right: Camera
+        ):
+        self.left = left
+        self.right = right
+    
+    @classmethod
+    def build(cls, prim_path: str) -> "HawkCamera":
+        
+        stage = get_stage()
+
+        stage_add_usd_ref(
+            stage=stage,
+            path=prim_path,
+            usd_path=cls.usd_url
+        )
+
+        return cls.attach(prim_path)
+    
+    @classmethod
+    def attach(cls, prim_path: str) -> "HawkCamera":
+        
+        left_camera = Camera(os.path.join(prim_path, cls.left_camera_path), cls.resolution)
+        right_camera = Camera(os.path.join(prim_path, cls.right_camera_path), cls.resolution)
+
+        return HawkCamera(left_camera, right_camera)
