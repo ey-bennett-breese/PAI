5edca09eafe9ebbdc0c2ed41170fb22b8c854e40
bennett <bennett.breese@ey.com>
2025-10-15 10:49:28 -0400

    new yolov8 build

--- Tank/ros2_ws/build/yolov8_detector/prefix_override/sitecustomize.py
+++ Tank/ros2_ws/build/yolov8_detector/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/eyadmin/ros2_ws/install/yolov8_detector'
+    sys.prefix = sys.exec_prefix = '/home/bennett/github/PAI/Tank/ros2_ws/install/yolov8_detector'
--- Tank/ros2_ws/src/build/yolov8_detector/prefix_override/sitecustomize.py
+++ Tank/ros2_ws/src/build/yolov8_detector/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/eyadmin/Desktop/Bennett/debug/ros2_ws/src/install/yolov8_detector'
+    sys.prefix = sys.exec_prefix = '/home/bennett/github/PAI/Tank/ros2_ws/src/install/yolov8_detector'
