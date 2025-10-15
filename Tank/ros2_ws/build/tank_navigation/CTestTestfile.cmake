6b5ec4d27431b9d4f85dab904d3f961b3a875ad1
bennett <bennett.breese@ey.com>
2025-10-15 10:49:53 -0400

    new yolov8 build

---
+++ Tank/ros2_ws/build/tank_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bennett/github/PAI/Tank/ros2_ws/src/navigation/tank_navigation
+# Build directory: /home/bennett/github/PAI/Tank/ros2_ws/build/tank_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
